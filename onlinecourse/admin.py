ae8da1fe4e38ca5355aaa2e3f41129f7b6a1a590
A-4S <dev.forsythe@gmail.com>
2026-06-23 16:38:29 -0400

    Updated admin.py; added ChoiceInline and QuestionInline classes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -23,6 +23,14 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
