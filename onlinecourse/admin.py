9e205be3d86c1fa1b70f622e3219b05031d61922
A-4S <dev.forsythe@gmail.com>
2026-06-23 16:42:26 -0400

    Updated admin.py; 1. added QuestionAdmin 2. registered Question, QuestionAdmin, Choice, and Submission

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -32,7 +32,16 @@ class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
